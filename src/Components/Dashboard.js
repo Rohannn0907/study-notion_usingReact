0c0f475fe28b10fb9b8f8b73042f8494ea486e56
Rohan <rgns0907@gmail.com>
2026-02-19 10:24:21 +0530

    Fix typo in welcome message on Dashboard

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -3,9 +3,10 @@ import React from 'react'
 const Dashboard = () => {
   return (
      <div className='grid place-items-center text-white text-3xl h-full py-48'>
-Welcome to Study Notation
+Welcome to Study Notion
       </div>
   )
 }
 
 export default Dashboard
+
